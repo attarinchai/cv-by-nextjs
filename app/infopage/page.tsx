4ce068a674f5a9988c64df43ed180c5d3c4b177b
attarinchai <attarinchai_ru@outlook.co.th>
2025-02-27 18:50:15 +0700

    Add files via upload

--- app/infopage/page.tsx
+++ app/infopage/page.tsx
@@ -1,8 +1,17 @@
 import portrait from '@/public/me.jpg'
 import Image from 'next/image'
+import Link from 'next/link'
 export default function Page(){
   return (
     <div className="m-auto w-full tracking-wide">
+      <div className='w-full fixed bottom-0 text-center'>
+        <p className='bg-gray-300'>see transcript
+          <Link className="text-blue-800 underline ml-2" key="transcript" href='transcript.pdf' target="_blank">link</Link>
+        </p>
+        <p className="bg-gray-400">see toeic score report(expired)
+          <Link className="text-blue-800 underline ml-2" key="toeic" href="toeic_report.jpg" target="_blank">link</Link>
+        </p>
+      </div>
       <div className='sm:flex sm:justify-center gap-[5%]'>
         <Image src={portrait} 
                alt='its him' 
